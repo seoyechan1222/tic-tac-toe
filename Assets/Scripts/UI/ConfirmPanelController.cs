3a9107e052a40ff755ba33a4882c8a93bc6e94e2
Junil Ko <goyadb@outlook.com>
2025-02-05 15:51:27 +0900

    ConfirmPanelController 수정

--- Assets/Scripts/UI/ConfirmPanelController.cs
+++ Assets/Scripts/UI/ConfirmPanelController.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class ConfirmPanelController : PanelController
 {
+    [SerializeField] private TMP_Text messageText;
+
+    public delegate void OnConfirmButtonClick();
+    private OnConfirmButtonClick onConfirmButtonClick;
+
+    public void Show(string message, OnConfirmButtonClick onConfirmButtonClick, OnHide onHide)
+    {
+        messageText.text = message;
+        this.onConfirmButtonClick = onConfirmButtonClick;
+        base.Show(onHide);
+    }
+    
     /// <summary>
     /// Confirm 버튼 클릭시 호출되는 함수
     /// </summary>
     public void OnClickConfirmButton()
     {
-        GameManager.Instance.InitGame();
+        onConfirmButtonClick?.Invoke();
         Hide();
     }
 
