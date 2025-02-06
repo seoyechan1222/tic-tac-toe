a57f560cd603896ab746b9d8337fa65540e6f13e
Junil Ko <goyadb@outlook.com>
2025-02-06 13:07:42 +0900

    SettingsPanel 구현 중

--- Assets/Scripts/UI/PanelController.cs
+++ Assets/Scripts/UI/PanelController.cs
@@ -24,8 +24,8 @@ public class PanelController : MonoBehaviour
         _backgroundCanvasGroup.alpha = 0;
         panelRectTransform.localScale = Vector3.zero;
         
-        _backgroundCanvasGroup.DOFade(1, 0.5f).SetEase(Ease.Linear);
-        panelRectTransform.DOScale(1, 0.5f).SetEase(Ease.InOutBack);
+        _backgroundCanvasGroup.DOFade(1, 0.3f).SetEase(Ease.Linear);
+        panelRectTransform.DOScale(1, 0.3f).SetEase(Ease.OutBack);
     }
 
     /// <summary>
@@ -33,6 +33,11 @@ public class PanelController : MonoBehaviour
     /// </summary>
     public void Hide()
     {
-        Destroy(gameObject);
+        _backgroundCanvasGroup.alpha = 1;
+        panelRectTransform.localScale = Vector3.one;
+        
+        _backgroundCanvasGroup.DOFade(0, 0.3f).SetEase(Ease.Linear);
+        panelRectTransform.DOScale(0, 0.3f)
+            .SetEase(Ease.InBack).OnComplete(() => Destroy(gameObject));
     }
 }
